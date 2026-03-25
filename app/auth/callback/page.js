2ea7397a8931a0edfd010d34f9a4f8c69a808f1f
kishore-techbium <kishore@techbium.com>
2026-03-25 15:12:44 +0530

    Update page.js

--- app/auth/callback/page.js
+++ app/auth/callback/page.js
@@ -106,7 +106,7 @@ export default function AuthCallback() {
         router.replace('/student/profile')
       } else {
         console.log("🟢 Profile complete → dashboard")
-        router.replace('/student/dashboard')
+        router.replace('/student/select-category')
       }
 
       return
