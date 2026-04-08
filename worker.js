84ec53285bd0581931bc76bbdadbc742eb70731e
kishore-techbium <kishore@techbium.com>
2026-04-08 16:32:28 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import { createClient } from '@supabase/supabase-js'
-
 const supabaseUrl =
   process.env.SUPABASE_URL || "https://huzzdveynwzcrpmzltrf.supabase.co"
 
