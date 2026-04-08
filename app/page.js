ef791d9166e8a86007c897dc798c75c89f8cc05e
kishore-techbium <kishore@techbium.com>
2026-04-08 14:37:06 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -31,7 +31,7 @@ async function checkUser() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: window.location.origin + '/auth/callback'
+         redirectTo: 'https://examzcanvas.com/auth/callback'
       }
     })
   }
