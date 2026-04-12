e06edfed8da101c6646e88bae289548b739941e2
kishore-techbium <kishore@techbium.com>
2026-04-12 10:56:52 +0530

    Update page.js

--- app/admin/results/[examId]/page.js
+++ app/admin/results/[examId]/page.js
@@ -219,7 +219,7 @@ else setDifficulty('Easy')
   submitted.forEach(s => {
     const totalQ = Object.keys(s.answers || {}).length || 1
 const maxScore = totalQ * (exam?.correct_marks || 4)
-const percent = (s.score / maxScore) * 100
+const percent = Math.max(0, (s.score / maxScore) * 100)
     if (percent <= 25) buckets['0-25']++
     else if (percent <= 50) buckets['26-50']++
     else if (percent <= 75) buckets['51-75']++
@@ -380,7 +380,7 @@ const percent = (s.score / maxScore) * 100
         const totalQ = Object.keys(s.answers || {}).length || 1
         const maxScore = totalQ * (exam.correct_marks || 4)
 
-        const percent = (s.score / maxScore) * 100
+        const percent = Math.max(0, (s.score / maxScore) * 100)
         return total + percent
       }, 0) / submitted.length
     ).toFixed(1)
