1125a02cb6c2fac69326db77e8783434c3ab6839
kishore-techbium <kishore@techbium.com>
2026-04-25 11:37:18 +0530

    Update page.js

--- app/admin/students/[studentId]/analysis/page.js
+++ app/admin/students/[studentId]/analysis/page.js
@@ -258,7 +258,7 @@ const strongest =
                         >
                           {s.accuracy.toFixed(1)}%
                         </span>{' '}
-                        ({s.attempts} Accuracy)
+                        ({s.attempts})
                       </div>
                     ))}
                   </div>
