999a4d9169537f4757b0434581bff4bfa33a5c5e
kishore-techbium <kishore@techbium.com>
2026-04-26 16:40:31 +0530

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -319,22 +319,10 @@ const questionCountMap = await res2.json()
         is_global: true
       }
     })
-console.log("MERGED BEFORE FILTER:", assignments.map(a => ({
-  assignment: a,
-  exam: exams.find(e => e.id === a.exam_id)
-})))
+
 .filter(e => {
   const matchCategory = allowedCategories.includes(e.exam_category)
   const matchYear = Number(e.target_year) === Number(userData.study_year)
-
-  console.log("FILTER CHECK:", {
-    exam_category: e.exam_category,
-    allowed: allowedCategories,
-    matchCategory,
-    target_year: e.target_year,
-    student_year: userData.study_year,
-    matchYear
-  })
 
   return matchCategory && matchYear
 })
