c90f80704da60970cbb8dc64bf7c6e2c3460e2c7
kishore-techbium <kishore@techbium.com>
2026-04-27 15:21:50 +0530

    Update route.js

--- app/api/admin/results/route.js
+++ app/api/admin/results/route.js
@@ -1,5 +1,5 @@
+export const dynamic = 'force-dynamic'
 import { createClient } from '@supabase/supabase-js'
-
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY // 🔥 bypass RLS safely
