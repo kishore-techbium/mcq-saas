d1b8c6e2c1b17ca43148a79ecb14a7835efd36e4
kishore-techbium <kishore@techbium.com>
2026-05-07 19:40:58 +0530

    Update route.js

--- app/api/exam/list/route.js
+++ app/api/exam/list/route.js
@@ -38,15 +38,27 @@ console.log('REQUEST DATA:', {
 let query = supabase.from('exams').select('*')
 
 if (examIds.length > 0) {
-  const formattedIds = examIds.map(id => `"${id}"`).join(',')
+
+  const formattedIds =
+    examIds.map(id => `"${id}"`).join(',')
 
   query = query.or(
-    `and(college_id.eq.${collegeId},is_active.eq.true),id.in.(${formattedIds})`
+    `
+    and(college_id.eq.${collegeId},is_active.eq.true),
+    and(is_global.eq.true,is_active.eq.true),
+    id.in.(${formattedIds})
+    `
   )
+
 } else {
-  query = query
-    .eq('college_id', collegeId)
-    .eq('is_active', true)
+
+  query = query.or(
+    `
+    and(college_id.eq.${collegeId},is_active.eq.true),
+    and(is_global.eq.true,is_active.eq.true)
+    `
+  )
+
 }
 
     const { data: exams, error } = await query
