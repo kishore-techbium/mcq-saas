e5dff56eb9b2a4bb19cbc37894f4038ede7cc27f
kishore-techbium <kishore@techbium.com>
2026-05-11 10:12:18 +0530

    Update route.js

--- app/api/anse/generate-rankings/route.js
+++ app/api/anse/generate-rankings/route.js
@@ -4,10 +4,6 @@ import {
   calculateExamMetrics
 } from '../../../../lib/anseExamMetrics'
 
-import {
-  calculateExamMetrics
-} from '@/lib/anseExamMetrics'
-
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
