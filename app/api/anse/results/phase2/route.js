9977138e170e1dae0417a491d9ffd77ec836ddb9
kishore-techbium <kishore@techbium.com>
2026-05-11 13:00:35 +0530

    Update route.js

--- app/api/anse/results/phase2/route.js
+++ app/api/anse/results/phase2/route.js
@@ -1,1 +1,8 @@
+export const dynamic = 'force-dynamic'
 
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+)
