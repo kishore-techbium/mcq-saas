26ffd185dafd37da451eb67a7357eeb8d6b00415
kishore-techbium <kishore@techbium.com>
2026-05-13 21:17:38 +0530

    Update page.js

--- app/exam/[id]/page.js
+++ app/exam/[id]/page.js
@@ -69,9 +69,7 @@ export default function ExamPage({ params }) {
   const [loading, setLoading] = useState(true)
   const [showConfirm, setShowConfirm] = useState(false)  
   const [showCertificate, setShowCertificate] = useState(false)
-  console.log('showCertificate:', showCertificate)
-console.log('submitted:', submitted)
-console.log('exam:', exam)
+  
     // 🔐 Integrity tracking (SAFE - no side effects)
   const [tabSwitchCount, setTabSwitchCount] = useState(0)
   const [blurCount, setBlurCount] = useState(0)
@@ -229,14 +227,11 @@ if (!currentUser) {
 })
 
 const data = await res.json()
-console.log('API DATA:', data)
+
 setShowCertificate(
   data.isSchoolStudent || false
 )
-console.log(
-  'SETTING SHOW CERTIFICATE:',
-  data.isSchoolStudent
-)    
+    
 if (!res.ok || !data || data.error) {
   alert('Unable to load exam')
   window.location.href = '/dashboard'
@@ -663,15 +658,6 @@ if (submitted) {
   {/* ======================================================
      DIGITAL CERTIFICATE
   ====================================================== */}
-
-  {console.log(
-  'RENDER CHECK:',
-  {
-    showCertificate,
-    submitted
-  }
-)}
-
 {showCertificate && (
 
     <button
