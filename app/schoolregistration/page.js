2dea94f2324c4cccb688bf5508a0b5892464ef15
kishore-techbium <kishore@techbium.com>
2026-05-14 11:29:34 +0530

    Update page.js

--- app/schoolregistration/page.js
+++ app/schoolregistration/page.js
@@ -5,6 +5,46 @@ import "./page.css";
 import { useState } from "react";
 import { supabase } from "../../lib/supabase";
 
+const INDIAN_STATES = [
+
+  "Andhra Pradesh",
+  "Arunachal Pradesh",
+  "Assam",
+  "Bihar",
+  "Chhattisgarh",
+  "Goa",
+  "Gujarat",
+  "Haryana",
+  "Himachal Pradesh",
+  "Jharkhand",
+  "Karnataka",
+  "Kerala",
+  "Madhya Pradesh",
+  "Maharashtra",
+  "Manipur",
+  "Meghalaya",
+  "Mizoram",
+  "Nagaland",
+  "Odisha",
+  "Punjab",
+  "Rajasthan",
+  "Sikkim",
+  "Tamil Nadu",
+  "Telangana",
+  "Tripura",
+  "Uttar Pradesh",
+  "Uttarakhand",
+  "West Bengal",
+
+  "Andaman and Nicobar Islands",
+  "Chandigarh",
+  "Dadra and Nagar Haveli and Daman and Diu",
+  "Delhi",
+  "Jammu and Kashmir",
+  "Ladakh",
+  "Lakshadweep",
+  "Puducherry"
+]
 export default function SchoolRegistrationPage() {
 
   const [loading, setLoading] = useState(false);
@@ -258,14 +298,31 @@ export default function SchoolRegistrationPage() {
                 State*
               </label>
 
-              <input
-                type="text"
-                name="state"
-                value={form.state}
-                onChange={handleChange}
-                placeholder="State"
-                required
-              />
+            <select
+  name="state"
+  value={form.state}
+  onChange={handleChange}
+  required
+>
+
+  <option value="">
+    Select State
+  </option>
+
+  {INDIAN_STATES.map(state => (
+
+    <option
+      key={state}
+      value={state}
+    >
+
+      {state}
+
+    </option>
+
+  ))}
+
+</select>
 
             </div>
 
