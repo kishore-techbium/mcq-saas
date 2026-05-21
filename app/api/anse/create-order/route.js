58297758dc1a119a5c368c540f26aec14dca5e94
kishore-techbium <kishore@techbium.com>
2026-05-21 13:38:30 +0530

    Update route.js

--- app/api/anse/create-order/route.js
+++ app/api/anse/create-order/route.js
@@ -6,25 +6,32 @@ const razorpay = new Razorpay({
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
-export async function POST(req) {
+export async function POST() {
+
   try {
-    const body = await req.json();
 
     const options = {
-      amount: body.amount * 100, // convert to paise
+      amount: 9900,
       currency: "INR",
       receipt: `receipt_${Date.now()}`,
     };
 
-    const order = await razorpay.orders.create(options);
+    const order =
+      await razorpay.orders.create(options);
 
     return NextResponse.json(order);
+
   } catch (error) {
-    console.error(error);
+
+    console.log("RAZORPAY ERROR:", error);
 
     return NextResponse.json(
-      { error: "Failed to create order" },
-      { status: 500 }
+      {
+        error: "Failed to create order",
+      },
+      {
+        status: 500,
+      }
     );
   }
 }
