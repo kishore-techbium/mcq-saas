0cb9672636f60d3eadc1cb217a76f156909c9e1f
kishore-techbium <kishore@techbium.com>
2026-05-21 13:23:01 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,25 @@
-export const metadata = {
-  title: 'Examination',
-  description: 'Online Exam Portal',
-}
+import Script from "next/script";
 
-export default function RootLayout({ children }) {
- return (
+export const metadata = {
+  title: "Examination",
+  description: "Online Exam Portal",
+};
+
+export default function RootLayout({
+  children,
+}) {
+
+  return (
     <html lang="en">
-      <body>{children}</body>
+
+      <body>
+
+        <Script src="https://checkout.razorpay.com/v1/checkout.js" />
+
+        {children}
+
+      </body>
+
     </html>
-  )
+  );
 }
