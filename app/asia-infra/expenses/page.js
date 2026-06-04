698d13a605d8b52eca0d7b7b8e573fe2d3aa1a9a
kishore-techbium <kishore@techbium.com>
2026-06-04 18:34:38 +0530

    Create page.js

---
+++ app/asia-infra/expenses/page.js
@@ -0,0 +1,18 @@
+export default function ExpensesPage() {
+
+  return (
+
+    <div>
+
+      <h1>
+        Expenses
+      </h1>
+
+      <p>
+        Excel-style entry screen
+        coming next.
+      </p>
+
+    </div>
+  )
+}
