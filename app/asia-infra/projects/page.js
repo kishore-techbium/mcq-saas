093b8a99f97f64fbce0ad2df505748ebfe6c2526
kishore-techbium <kishore@techbium.com>
2026-06-04 18:34:08 +0530

    Create page.js

---
+++ app/asia-infra/projects/page.js
@@ -0,0 +1,17 @@
+export default function ProjectsPage() {
+
+  return (
+
+    <div>
+
+      <h1>
+        Projects
+      </h1>
+
+      <button>
+        + New Project
+      </button>
+
+    </div>
+  )
+}
